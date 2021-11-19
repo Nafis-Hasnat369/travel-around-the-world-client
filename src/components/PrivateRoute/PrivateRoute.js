2e4eab4a38947c853d1608104142b206cbf838ba
Nafis-Hasnat369 <nafis7823@gmail.com>
2021-11-19 21:54:35 +0600

    fixed

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,13 +1,9 @@
-import { CircularProgress } from '@mui/material';
 import React from 'react';
 import { Redirect, Route } from 'react-router';
 import useAuth from '../../Hooks/useAuth';
 
 const PrivateRoute = ({ children, ...rest }) => {
     const { user } = useAuth();
-    if (user.email) {
-        return <CircularProgress />
-    }
     return (
         <Route
             {...rest}
